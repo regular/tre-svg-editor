a24aa9594045aadb449014c7b9e3bfa99eeb1fb1
Jan Bölsche <regular.gonzales@googlemail.com>
2021-09-13 11:36:16 +0200

    Use svgo to prettify, too. Remove nxmlpp dep

--- prettify.js
+++ prettify.js
@@ -1,5 +1,11 @@
-const format = require('nxmlpp').strPrint
+const {optimize} = require('svgo')
 
-module.exports = function(xml) {
-  return format(xml)
+module.exports = function(svg) {
+  return optimize(svg, {
+    js2svg: {
+      indent: 4, // string with spaces or number of spaces. 4 by default
+      pretty: true, // boolean, false by default
+    },
+    plugins: []
+  }).data
 }
